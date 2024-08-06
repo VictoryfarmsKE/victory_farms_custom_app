73af3c6ac6ddc6f53f7b70254f1e568057ab4c38
Sahil Suthar <sahilsuthar607@gmail.com>
2024-08-06 11:50:47 +0530

    fix: auto removed added in SS

--- victory_farms_custom_app/victory_farms_custom_app/customization/salary_slip/salary_slip.py
+++ victory_farms_custom_app/victory_farms_custom_app/customization/salary_slip/salary_slip.py
@@ -3,6 +3,61 @@ from frappe.utils import flt
 from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
 
 class CustomSalarySlip(SalarySlip):
+	def add_structure_components(self, component_type):
+		self.data, self.default_data = self.get_data_for_eval()
+
+		timesheet_component = frappe.db.get_value(
+			"Salary Structure", self.salary_structure, "salary_component"
+		)
+
+		for struct_row in self._salary_structure_doc.get(component_type):
+			if self.salary_slip_based_on_timesheet and struct_row.salary_component == timesheet_component:
+				continue
+			amount = self.eval_condition_and_formula(struct_row, self.data)
+			# if struct_row.parentfield == "deductions" and struct_row.salary_component == "Insurance Relief":
+			# 	frappe.throw(f"{amount}")
+			if struct_row.statistical_component:
+				# update statitical component amount in reference data based on payment days
+				# since row for statistical component is not added to salary slip
+
+				self.default_data[struct_row.abbr] = flt(amount)
+				if struct_row.depends_on_payment_days:
+					joining_date, relieving_date = self.get_joining_and_relieving_dates()
+					payment_days_amount = (
+						flt(amount) * flt(self.payment_days) / cint(self.total_working_days)
+						if self.total_working_days
+						else 0
+					)
+					self.data[struct_row.abbr] = flt(payment_days_amount, struct_row.precision("amount"))
+
+			else:
+				# default behavior, the system does not add if component amount is zero
+				# if remove_if_zero_valued is unchecked, then ask system to add component row
+				remove_if_zero_valued = frappe.get_cached_value(
+					"Salary Component", struct_row.salary_component, "remove_if_zero_valued"
+				)
+
+				default_amount = 0
+
+				for row in self.get(component_type):
+					if not amount and row.get("salary_component") == struct_row.salary_component:
+						self.remove(row)
+
+				if (
+					amount
+					or (struct_row.amount_based_on_formula and amount is not None)
+					or (not remove_if_zero_valued and amount is not None and not self.data[struct_row.abbr])
+				):
+					default_amount = self.eval_condition_and_formula(struct_row, self.default_data)
+					self.update_component_row(
+						struct_row,
+						amount,
+						component_type,
+						data=self.data,
+						default_amount=default_amount,
+						remove_if_zero_valued=remove_if_zero_valued,
+					)
+
 	def update_component_row(
 		self,
 		component_data,
