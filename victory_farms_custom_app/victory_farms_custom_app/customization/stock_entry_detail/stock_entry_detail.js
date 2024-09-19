5e06f945e9fcec8414dcce6ff279642959ffb003
AmmarOfficewalaSerpentCS <ammar.o.serpentcs@gmail.com>
2024-09-19 12:02:28 +0530

    [FIX] Fixed issue of production console.

--- victory_farms_custom_app/victory_farms_custom_app/customization/stock_entry_detail/stock_entry_detail.js
+++ victory_farms_custom_app/victory_farms_custom_app/customization/stock_entry_detail/stock_entry_detail.js
@@ -1,27 +1,25 @@
 frappe.ui.form.on('Stock Entry Detail', {
-	custom_get_weights_: function(frm, cnd, cdt) {
-           $.ajax({
-	type: 'POST',
-	url: "http:localhost:5000/get_weight_scale",
-	data: {},
-	success: function (data, status, xhr) {
-		console.log("/////////////", data)
+	custom_get_weights_: function (frm, cnd, cdt) {
+		$.ajax({
+			type: 'POST',
+			url: "http:localhost:5000/get_weight_scale",
+			data: {},
+			success: function (data, status, xhr) {
+				if (data.error == '') {
+					$.each(frm.doc.items || [], function (i, v) {
+						if (cdt == v.name) {
+							frappe.model.set_value(v.doctype, v.name, "qty", data.data)
+						}
 
-                if (data.error == ''){
-		$.each(frm.doc.items || [], function(i, v) {
-			console.log("//////////////////", cdt, v.name)
-			if (cdt == v.name){
-			   frappe.model.set_value(v.doctype, v.name, "qty", data.data)
+					})
+				}
+
+			},
+			error: function (xhr, status, error) {
+				console.error(xhr);
+				console.error(status);
+				console.error(error);
 			}
-
-		})}
-
-	},
-	error: function (xhr, status, error) {
-		console.log(xhr);
-		console.log(status);
-		console.log(error);
-	}
-});
+		});
 	}
 })
