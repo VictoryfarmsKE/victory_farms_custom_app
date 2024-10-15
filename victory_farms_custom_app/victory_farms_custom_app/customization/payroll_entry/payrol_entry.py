f3429e798850242429018a18466d64bdb0ac9def
Sahil Suthar <sahilsuthar607@gmail.com>
2024-10-16 00:51:42 +0530

    fix: currency related changes added in payroll

--- victory_farms_custom_app/victory_farms_custom_app/customization/payroll_entry/payrol_entry.py
+++ victory_farms_custom_app/victory_farms_custom_app/customization/payroll_entry/payrol_entry.py
@@ -1,4 +1,5 @@
 import frappe
+import erpnext
 from frappe.utils import add_days
 from hrms.payroll.doctype.payroll_entry.payroll_entry import PayrollEntry, get_filter_condition, get_joining_relieving_condition, get_emp_list, remove_payrolled_employees, get_salary_structure
 
@@ -19,12 +20,18 @@ class CustomPayrollEntry(PayrollEntry):
 			sal_struct = get_salary_structure(
 				self.company, self.currency, self.salary_slip_based_on_timesheet, self.payroll_frequency
 			)
+
+		if erpnext.get_company_currency(self.company) != self.currency:
+			cond += "and t2.salary_currency = %(salary_currency)s "
+			cond += "and t2.payroll_payable_account = %(payroll_payable_account)s "
+			cond += "and %(from_date)s >= t2.from_date"
+			emp_list = get_other_currency_emp(cond, self.currency, self.end_date, self.payroll_payable_account)
+			return emp_list
+	
 		if sal_struct:
 			cond += "and t2.salary_structure IN %(sal_struct)s "
 			cond += "and t2.payroll_payable_account = %(payroll_payable_account)s "
 			cond += "and %(from_date)s >= t2.from_date"
-			if self.salary_currency:
-				cond += f" and t1.salary_currency = '{self.salary_currency}'"
 			emp_list = get_emp_list(sal_struct, cond, self.end_date, self.payroll_payable_account)
 			emp_list = remove_wrong_ssa_applied(emp_list, self.start_date, self.end_date)
 			emp_list = remove_payrolled_employees(emp_list, self.start_date, self.end_date)
@@ -45,3 +52,25 @@ def remove_wrong_ssa_applied(emp_list, start_date, end_date):
 			new_emp_list.append(employee_details)
 
 	return new_emp_list
+
+def get_other_currency_emp(cond, salary_currency, end_date, payroll_payable_account):
+	return frappe.db.sql(
+		"""
+			select
+				distinct t1.name as employee, t1.employee_name, t1.department, t1.designation
+			from
+				`tabEmployee` t1, `tabSalary Structure Assignment` t2
+			where
+				t1.name = t2.employee
+				and t2.docstatus = 1
+				and t1.status != 'Inactive'
+		%s order by t2.from_date desc
+		"""
+		% cond,
+		{
+			"salary_currency": salary_currency,
+			"from_date": end_date,
+			"payroll_payable_account": payroll_payable_account,
+		},
+		as_dict=True,
+	)
