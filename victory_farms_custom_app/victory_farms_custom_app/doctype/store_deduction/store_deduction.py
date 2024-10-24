cee99546fc6f84e8a4f712f149df49f21582a260
Sahil Suthar <sahilsuthar607@gmail.com>
2024-10-24 22:48:45 +0530

    fix: changes added for Store Deduction

--- victory_farms_custom_app/victory_farms_custom_app/doctype/store_deduction/store_deduction.py
+++ victory_farms_custom_app/victory_farms_custom_app/doctype/store_deduction/store_deduction.py
@@ -12,18 +12,18 @@ class StoreDeduction(Document):
 		payroll_date = get_last_day(self.posting_date)
 		curr_month_last_date = get_last_day(today())
 
-		total_range = month_diff(payroll_date, curr_month_last_date)
+		total_range = month_diff(curr_month_last_date, payroll_date)
 
-		if total_range > 1:
-			total_range -= 1
-
-		if total_range > 5:
-			total_range = 5
+		divide_cost = total_range > 1
+		total_range = min(total_range - 1, 5) if total_range > 1 else total_range
 
 		for i in range(total_range):
-			if ads_name:= frappe.db.get_value("Additional Salary", {"docstatus": 0, "from_date": payroll_date, "salary_component": salary_component, "employee": self.employee}):
+			item_cost = self.item_cost
+			if divide_cost:
+				item_cost /= 5
+			if ads_name:= frappe.db.get_value("Additional Salary", {"docstatus": 0, "payroll_date": payroll_date, "salary_component": salary_component, "employee": self.employee}):
 				ads_doc = frappe.get_doc("Additional Salary", ads_name)
-				ads_doc.amount += self.item_cost
+				ads_doc.amount += item_cost
 				ads_doc.save()
 				self.db_set("remaining_payments", self.remaining_payments - 1)
 			
@@ -33,11 +33,14 @@ class StoreDeduction(Document):
 				ads_doc.employee = self.employee
 				ads_doc.payroll_date = payroll_date
 				ads_doc.currency = frappe.db.get_value("Employee", self.employee, "salary_currency")
-				ads_doc.amount = self.item_cost
+				ads_doc.amount = item_cost
 				ads_doc.overwrite_salary_structure_amount = 1
 				ads_doc.save()
 
-				self.db_set("remaining_payments", self.period_of_payment - 1)
+				if i == 0:
+					self.db_set("remaining_payments", self.period_of_payment - 1)
+				else:
+					self.db_set("remaining_payments", self.remaining_payments - 1)
 			payroll_date = get_last_day(add_days(payroll_date, days = 1))
 
 
