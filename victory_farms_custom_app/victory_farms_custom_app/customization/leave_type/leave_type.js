a4a2a2a48cdbd8d0ef15413b4a3d37ca0b5914ee
Savan120 <patelsavan931406891@gmail.com>
2025-02-13 14:07:32 +0530

    fix: leave schedular changes added

--- victory_farms_custom_app/victory_farms_custom_app/customization/leave_type/leave_type.js
+++ victory_farms_custom_app/victory_farms_custom_app/customization/leave_type/leave_type.js
@@ -3,7 +3,9 @@ frappe.ui.form.on("Leave Type", {
         frm.add_custom_button(__("Create Leave Allocation"), function () {
             frappe.call({
                 method: "victory_farms_custom_app.victory_farms_custom_app.customization.leave_type.leave_type.create_leave_allocation",
-                args: { leave_type: frm.doc.name },
+                args: { leave_type: frm.doc.name, is_earned_leave: frm.doc.is_earned_leave},
+                freeze: true,
+                freeze_message: __("Creating Leave Allocations"),
                 callback: function () {
                     frappe.msgprint(__("Leave Allocation has been generated."));
                 },
