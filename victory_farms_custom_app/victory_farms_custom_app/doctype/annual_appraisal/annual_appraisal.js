c8eaca4b916d28d8c1a07c2c213b4382774a498b
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2026-01-05 11:37:03 +0300

    fix: annual appraisal fetch

--- victory_farms_custom_app/victory_farms_custom_app/doctype/annual_appraisal/annual_appraisal.js
+++ victory_farms_custom_app/victory_farms_custom_app/doctype/annual_appraisal/annual_appraisal.js
@@ -16,9 +16,49 @@ frappe.ui.form.on("Annual Appraisal", {
             },
         );
     },
+
+    employee(frm) {
+        // Fetch bonus potential fields when employee changes
+        if (frm.doc.employee) {
+            frappe.db.get_value("Employee", frm.doc.employee, 
+                ["bonus_potential", "custom_department_bonus_potential_", "custom_companygroup_bonus", "department"],
+                function(r) {
+                    if (r) {
+                        frm.set_value("bonus_potential", r.bonus_potential || 0);
+                        frm.set_value("bonus_potential_department", r.custom_department_bonus_potential_ || 0);
+                        frm.set_value("bonus_potential_company", r.custom_companygroup_bonus || 0);
+                        frm.set_value("department", r.department || "");
+                        frm.refresh_fields();
+                    }
+                }
+            );
+        } else {
+            frm.set_value("bonus_potential", 0);
+            frm.set_value("bonus_potential_department", 0);
+            frm.set_value("bonus_potential_company", 0);
+            frm.set_value("department", "");
+            frm.refresh_fields();
+        }
+    },
+
+    onload(frm) {
+        // Fetch latest bonus potential values on form load for existing documents
+        if (frm.doc.employee && !frm.is_new()) {
+            frappe.db.get_value("Employee", frm.doc.employee, 
+                ["bonus_potential", "custom_department_bonus_potential_", "custom_companygroup_bonus"],
+                function(r) {
+                    if (r) {
+                        if (frm.doc.bonus_potential != r.bonus_potential ||
+                            frm.doc.bonus_potential_department != r.custom_department_bonus_potential_ ||
+                            frm.doc.bonus_potential_company != r.custom_companygroup_bonus) {
+                            
+                            frm.set_value("bonus_potential", r.bonus_potential || 0);
+                            frm.set_value("bonus_potential_department", r.custom_department_bonus_potential_ || 0);
+                            frm.set_value("bonus_potential_company", r.custom_companygroup_bonus || 0);
+                        }
+                    }
+                }
+            );
+        }
+    }
 });
-
-
-// frappe.ui.form.on("Quarterly Department Details", {
-//     pass
-// });
