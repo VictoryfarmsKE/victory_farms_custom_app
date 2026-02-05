00baeac2a0ca3e94afafa3074a9c43facc151fc4
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2026-02-05 11:40:15 +0300

    feat: bonus pay NCBA Bank Files

---
+++ victory_farms_custom_app/victory_farms_custom_app/report/ncba___otherbanks_pesalink_bonus_template/ncba___otherbanks_pesalink_bonus_template.js
@@ -0,0 +1,40 @@
+// Copyright (c) 2026, Solufy and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["NCBA - Otherbanks Pesalink Bonus Template"] = {
+"filters": [
+		{
+            "fieldname": "posting_date",
+            "label": __("Posting Date"),
+            "fieldtype": "Date",
+            "reqd": 1  
+        },
+        {
+            "fieldname": "company",
+            "label": __("Company"),
+            "fieldtype": "Link",
+            "options": "Company",
+            "reqd": 0,  
+            "default": frappe.defaults.get_default("company"),
+        },
+        {
+            "fieldname": "payout_frequency",
+            "label": __("Payout Frequency"),
+            "fieldtype": "Select",
+            "options": ["", "Monthly", "Quarterly", "Annually"],
+            "reqd": 1  
+        },
+		{
+			fieldname: "docstatus",	
+			label: __("Document Status"),
+			fieldtype: "Select",
+			options: [
+				{ "value": "", "label": __("All") },
+				{ "value": "0", "label": __("Draft") },
+				{ "value": "1", "label": __("Submitted") },
+				{ "value": "2", "label": __("Cancelled") },
+			],
+			default: "1",
+		}
+    ]
+};
