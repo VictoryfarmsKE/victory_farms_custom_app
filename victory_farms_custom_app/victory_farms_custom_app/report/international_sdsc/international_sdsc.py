ef6ef27d344f33b2551b70d299c24b4fcfb10c7f
ChristineKanga-VF <christinek@victoryfarmskenya.com>
2026-04-07 16:32:47 +0300

    feat: add custom transfer type filter for international and local transactions

--- victory_farms_custom_app/victory_farms_custom_app/report/international_sdsc/international_sdsc.py
+++ victory_farms_custom_app/victory_farms_custom_app/report/international_sdsc/international_sdsc.py
@@ -180,6 +180,7 @@ def get_detail_rows(from_date, to_date):
 		WHERE
 			ss.docstatus = 1
 			AND emp.salary_currency = 'USD'
+			AND emp.custom_transfer_type = 'International'
 			AND ss.posting_date BETWEEN %(from_date)s AND %(to_date)s
 		ORDER BY
 			emp.employee_name
